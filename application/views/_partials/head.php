87b5b05b20d82604c7ccff30e13cd57df3193344
chrstyzee7 <zee.christy7@gmail.com>
2025-02-25 10:39:51 +0700

    Update the layout of the username, factory, and user profile picture in `head.php`

--- application/views/_partials/head.php
+++ application/views/_partials/head.php
@@ -54,6 +54,25 @@
       filter: grayscale(100%) brightness(1000%);
     }
 
+    .user-menu {
+  display: d-flex;
+  align-items: center;
+  margin-top: 25px;
+}
+
+.user-menu .text-end {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end; /* Pastikan teks rata kanan */
+  margin-right: 10px; /* Jarak antara teks dan foto */
+}
+
+.user-menu img {
+  width: 40px;
+  height: 40px;
+  object-fit: cover;
+}
+
 
     </style>
 <!--=======-->
@@ -92,107 +111,106 @@
     <!--begin::App Wrapper-->
     <div class="app-wrapper">
       <!--begin::Header-->
-      <nav class="app-header navbar navbar-expand bg-body">
-        <!--begin::Container-->
-        <div class="container-fluid">
-          <!--begin::Start Navbar Links-->
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link" data-lte-toggle="sidebar" href="#" role="button" style="color: #001F82; font-weight: 800; font-size: 24px; margin-bottom: 5px;">
-                <i class="bi bi-list"></i> <?php echo $this->session->flashdata('page_title'); ?>
-              </a>
-            </li>
-          </ul>
-          <!--end::Start Navbar Links-->
-          <!--begin::End Navbar Links-->
-          <ul class="navbar-nav ms-auto">
-            <!--begin::Messages Dropdown Menu-->
-            <li class="nav-item dropdown">
-              <a class="nav-link" href="#">
-                <i class="fa-solid fa-location-dot" style="color: #001F82;"></i>
-                <span class="d-none d-md-inline"> Factory <?php echo $this->session->userdata('user_factory');?></span>
-                <!-- <span class="navbar-badge badge text-bg-danger">3</span> -->
-              </a>
-            </li>
-            <!--end::Messages Dropdown Menu-->
-            <!--begin::Notifications Dropdown Menu-->
-            <li class="nav-item dropdown">
-              <a class="nav-link" data-bs-toggle="dropdown" href="#">
-                <i class="bi bi-bell-fill" style="color: #001F82;"></i>
-                <span class="navbar-badge badge text-bg-warning">1</span>
-              </a>
-              <div class="dropdown-menu dropdown-menu-lg dropdown-menu-end">
-                <a href="#" class="dropdown-item">
-                  <!--begin::Message-->
-                  <div class="d-flex">
-                    <div class="flex-grow-1">
-                      <h3 class="dropdown-item-title">
-                        Brad Diesel
-                        <span class="float-end fs-7 text-danger"
-                          ><i class="bi bi-star-fill"></i
-                        ></span>
-                      </h3>
-                      <p class="fs-7">Call me whenever you can...</p>
-                      <p class="fs-7 text-secondary">
-                        <i class="bi bi-clock-fill me-1"></i> 4 Hours Ago
-                      </p>
-                    </div>
-                  </div>
-                  <!--end::Message-->
-                </a>
-                <div class="dropdown-divider"></div>
-                <div class="dropdown-divider"></div>
-                <a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
+<nav class="app-header navbar navbar-expand bg-body align-items-center">
+  <!--begin::Container-->
+  <div class="container-fluid">
+    <!--begin::Start Navbar Links-->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" data-lte-toggle="sidebar" href="#" role="button" style="color: #001F82; font-weight: 800; font-size: 24px; margin-bottom: 5px;">
+          <i class="bi bi-list"></i> <?php echo $this->session->flashdata('page_title'); ?>
+        </a>
+      </li>
+    </ul>
+    <!--end::Start Navbar Links-->
+
+    <!--begin::End Navbar Links-->
+    <ul class="navbar-nav ms-auto align-items-center">
+      <!--begin::Notifications Dropdown Menu-->
+      <li class="nav-item dropdown me-4">
+        <a class="nav-link" data-bs-toggle="dropdown" href="#">
+          <i class="bi bi-bell-fill fs-3 align-items-center" style="color: #001F82;"></i>
+          <span class="navbar-badge badge text-bg-warning fs-8">1</span>
+        </a>
+        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-end">
+          <a href="#" class="dropdown-item">
+            <div class="d-flex">
+              <div class="flex-grow-1">
+                <h3 class="dropdown-item-title">
+                  Brad Diesel
+                  <span class="float-end fs-7 text-danger">
+                    <i class="bi bi-star-fill"></i>
+                  </span>
+                </h3>
+                <p class="fs-7">Call me whenever you can...</p>
+                <p class="fs-7 text-secondary">
+                  <i class="bi bi-clock-fill me-1"></i> 4 Hours Ago
+                </p>
               </div>
-            </li>
-            <!--end::Notifications Dropdown Menu-->
-            <!--begin::Fullscreen Toggle-->
-            <!-- <li class="nav-item">
-              <a class="nav-link" href="#" data-lte-toggle="fullscreen">
-                <i data-lte-icon="maximize" class="bi bi-arrows-fullscreen"></i>
-                <i data-lte-icon="minimize" class="bi bi-fullscreen-exit" style="display: none"></i>
-              </a>
-            </li> -->
-            <!--end::Fullscreen Toggle-->
-            <!--begin::User Menu Dropdown-->
-            <li class="nav-item dropdown user-menu">
-              <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
-                <img
-                  src="<?= base_url('assets/dist/assets/img/user2-160x160.jpg');?>"
-                  class="user-image rounded-circle shadow"
-                  alt="User Image"
-                />
-                <span class="d-none d-md-inline" style="color: #001F82;"> <?php echo $this->session->userdata('user_name');?></span>
-              </a>
-              <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-end">
-                <!--begin::User Image-->
-                <li class="user-header text-bg-primary">
-                  <img
-                    src="<?= base_url('assets/dist/assets/img/user2-160x160.jpg');?>"
-                    class="rounded-circle shadow"
-                    alt="User Image"
-                  />
-                  <p>
-                    <?php echo $this->session->userdata('user_name');?>
-                    <small><?php echo $this->session->userdata('user_role');?>
-                    </small>
-                  </p>
-                </li>
-                <!--end::User Image-->
-                <!--begin::Menu Footer-->
-                <li class="user-footer">
-                  <a href="<?= site_url('auth/logout');?>" class="btn btn-default btn-flat float-end">Sign out</a>
-                </li>
-                <!--end::Menu Footer-->
-              </ul>
-            </li>
-            <!--end::User Menu Dropdown-->
-          </ul>
-          <!--end::End Navbar Links-->
+            </div>
+          </a>
+          <div class="dropdown-divider"></div>
+          <a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
         </div>
-        <!--end::Container-->
-      </nav>
-      <!--end::Header-->
+      </li>
+      <!--end::Notifications Dropdown Menu-->
+
+      <!--begin::User Info (Factory + Username)-->
+<li class="nav-item dropdown d-flex flex-column align-items-start me-4">
+
+    <span class="fw-bold text-primary">
+        <i class="fa-solid fa-location-dot me-2" style="color: #001F82;"></i> 
+        Factory <?php echo $this->session->userdata('user_factory'); ?>
+    </span>
+    <a href="#" class="dropdown-toggle text-primary fw-bold d-flex align-items-center text-decoration-none" data-bs-toggle="dropdown">
+        <i class="fa-solid fa-user-circle me-2" style="color: #001F82;"></i>
+        <?php echo $this->session->userdata('user_name'); ?>
+    </a>
+    
+
+    <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-end align-items-center">
+        <!--begin::User Image-->
+        <li class=" text-bg-primary text-center align-items-center user-menu" style="height: 150px;">
+            <img src="<?= base_url('assets/dist/assets/img/user2-160x160.jpg');?>" class="rounded-circle shadow" alt="User Image" style="width: 60px;height: 60px;"/>
+            <p>
+                <?php echo $this->session->userdata('user_name'); ?><br>
+                <small><?php echo $this->session->userdata('user_role'); ?></small>
+            </p>
+        </li>
+        <!--end::User Image-->
+        <!--begin::Menu Footer-->
+        <li class="user-footer text-center">
+            <a href="<?= site_url('auth/logout');?>" class="btn btn-flat btn-outline-primary w-100 rounded-0">Sign out</a>
+        </li>
+        <!--end::Menu Footer-->
+    </ul>
+</li>
+<!--end::User Info (Factory + Username)-->
+
+      <!--begin::User Image-->
+      <li class="nav-item align-items-center">
+        <img
+          src="<?= base_url('assets/dist/assets/img/user2-160x160.jpg');?>"
+          class="rounded-circle shadow"
+          alt="User Image"
+          style="width: 50px; height: 50px;"
+        />
+      </li>
+      <!--end::User Image-->
+    </ul>
+    <!--end::End Navbar Links-->
+  </div>
+  <!--end::Container-->
+</nav>
+<!--end::Header-->
+
+
+
+
+
+
+
+      
       <!--begin::Sidebar-->
       <aside class="app-sidebar bg-white shadow" data-bs-theme="light">
         <!--begin::Sidebar Brand-->
